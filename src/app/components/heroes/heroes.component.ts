ace94becbcb7ca9bbdd5a63503b7cceca466523b
nivaldojsneto <nivaldojsneto@gmail.com>
2022-01-30 16:01:06 -0300

    data bind

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Hero } from './../../hero.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss'],
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Goku',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
