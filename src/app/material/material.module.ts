f7b5c0f61d2e29a0ce43e278ec6992e0cc96ad8d
nivaldojsneto <nivaldojsneto@gmail.com>
2022-02-06 23:27:50 -0300

    loading service

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -20,6 +21,7 @@ const MODULES = [
   MatListModule,
   MatInputModule,
   MatTableModule,
+  MatProgressSpinnerModule,
 ];
 @NgModule({
   imports: [MODULES],
